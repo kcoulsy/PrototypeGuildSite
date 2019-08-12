0652d1468bd1c23aab2a35f01c4df87f7a6a08d3
Kristian <kristian@coulsy.co.uk>
2019-08-12 17:42:35 +0100

    added basic progress and recruitment apis

--- routes/routes.js
+++ routes/routes.js
@@ -6,4 +6,23 @@ const router = express.Router();
 
 router.post('/apply/submit', Apply.submit);
 
+const prog = require('../constants/progress');
+
+router.get('/progress', (req, res) => {
+    res.send(prog);
+});
+
+router.get('/recruitment', (req, res) => {
+    res.send({
+        warrior: 1,
+        paladin: 1,
+        hunter: 1,
+        druid: 1,
+        rogue: 1,
+        mage: 1,
+        priest: 1,
+        warlock: 1,
+    });
+});
+
 module.exports = router;
