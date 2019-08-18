e84951c672e5ed7405485553e86aadbbfd4632a4
Kristian <kristian@coulsy.co.uk>
2019-08-18 11:48:45 +0100

    Added input for importer

--- pages/manager/import.js
+++ pages/manager/import.js
@@ -1,4 +1,7 @@
 import React, { Component } from 'react';
+import JSONInput from 'react-json-editor-ajrm';
+import locale from 'react-json-editor-ajrm/locale/en';
+
 import { redirectIfNotAuthenticated, isAuthenticated } from '../../lib/auth';
 
 import ManagerContainer from '../../containers/ManagerContainer';
@@ -8,12 +11,26 @@ export default class Import extends Component {
         redirectIfNotAuthenticated(ctx);
         return {
             isAuthenticated: isAuthenticated(ctx),
+            entered: false
         };
+    }
+    state = {
+        data: {}
     }
     render() {
         return (
             <ManagerContainer {...this.props}>
                 <h2>Importer</h2>
+                <JSONInput
+                    id="player_importer"
+                    onChange={({jsObject}) =>
+                        this.setState({ data: jsObject, entered: true})
+                    }
+                    locale={locale}
+                    height="550px"
+                    width="100%"
+                />
+                { this.state.entered ? (<button>Save</button>) : null}
             </ManagerContainer>
         );
     }
