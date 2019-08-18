4e3d386e193c233ae9a6aa6dd183f832db7ba954
Kristian <kristian@coulsy.co.uk>
2019-08-18 11:13:40 +0100

    Extended schema validation and updated editor

--- pages/manager/schemas/create.js
+++ pages/manager/schemas/create.js
@@ -1,4 +1,7 @@
 import React, { Component } from 'react';
+import JSONInput from 'react-json-editor-ajrm';
+import locale from 'react-json-editor-ajrm/locale/en';
+
 import { redirectIfNotAuthenticated, isAuthenticated } from '../../../lib/auth';
 
 import validateSchema from '../../../lib/validateSchema';
@@ -13,33 +16,52 @@ export default class Create extends Component {
     }
     state = {
         schema: '',
-        errors: []
-    }
+        errors: [],
+        isValid: false,
+    };
     validateSchema = () => {
-        let errors = []
+        let errors = [];
+        let isValid = true;
         try {
             const schema = JSON.parse(this.state.schema);
             const validation = validateSchema(schema);
-            errors = [...validation.errors, ...errors]
-        } catch(e) {
-            console.log(e);
+            errors = [...validation.errors, ...errors];
+            isValid = validation.isValid;
+        } catch (e) {
             errors.push('Not Valid JSON');
+            isValid = false;
         }
-        this.setState({errors})
-    }
+        this.setState({ errors, isValid });
+    };
     render() {
-        console.log(this.state);
         return (
             <ManagerContainer {...this.props}>
                 <h2>Create Schema</h2>
-                <textarea onChange={(ev) => this.setState({schema: ev.currentTarget.value})}></textarea>
+                <JSONInput
+                    id="schema_inputter"
+                    onChange={({ json }) =>
+                        this.setState({ schema: json, isValid: false })
+                    }
+                    locale={locale}
+                    height="550px"
+                    width="100%"
+                />
                 <button onClick={this.validateSchema}>Validate</button>
                 <ul>
-
-                {this.state.errors.map(error => {
-                    return <li key={error.split(' ').join('_').toLowerCase()}>{error}</li>
-                })}
+                    {this.state.errors.map(error => {
+                        return (
+                            <li
+                                key={error
+                                    .split(' ')
+                                    .join('_')
+                                    .toLowerCase()}
+                            >
+                                {error}
+                            </li>
+                        );
+                    })}
                 </ul>
+                {this.state.isValid ? <button>Create</button> : null}
             </ManagerContainer>
         );
     }
