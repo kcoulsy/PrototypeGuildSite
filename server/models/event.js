d5ea5da6a19f8b09f31bbb0e8fcfd9ab2d464864
Kristian <kristian@coulsy.co.uk>
2019-08-19 18:14:59 +0100

    Added event model

---
+++ server/models/event.js
@@ -0,0 +1,16 @@
+'use strict';
+module.exports = (sequelize, DataTypes) => {
+    const Event = sequelize.define(
+        'Event',
+        {
+            name: DataTypes.STRING,
+            date: DataTypes.DATE,
+            schemaId: DataTypes.INTEGER,
+        },
+        {}
+    );
+    Event.associate = function(models) {
+        Event.hasOne(models.schema);
+    };
+    return Event;
+};
--- server/models/schema.js
+++ server/models/schema.js
@@ -1,12 +1,16 @@
 'use strict';
 module.exports = (sequelize, DataTypes) => {
-  const Schema = sequelize.define('Schema', {
-    name: DataTypes.STRING,
-    schema: DataTypes.TEXT,
-    enabled: DataTypes.BOOLEAN
-  }, {});
-  Schema.associate = function(models) {
-    // associations can be defined here
-  };
-  return Schema;
+    const Schema = sequelize.define(
+        'Schema',
+        {
+            name: DataTypes.STRING,
+            schema: DataTypes.TEXT,
+            enabled: DataTypes.BOOLEAN,
+        },
+        {}
+    );
+    Schema.associate = function(models) {
+        Schema.hasMany(models.event);
+    };
+    return Schema;
 };
