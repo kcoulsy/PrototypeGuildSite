719e69aa0bb12cce18cb34e2a2b5d4a231a0e10e
Kristian <kristian@coulsy.co.uk>
2019-08-19 20:40:23 +0100

    Added create event form

--- pages/manager/events/create.js
+++ pages/manager/events/create.js
@@ -3,6 +3,7 @@ import axios from 'axios';
 
 import { isAuthenticated } from '../../../src/lib/auth';
 import ManagerContainer from '../../../src/containers/ManagerContainer';
+import redirect from '../../../src/lib/redirect';
 
 export default class Create extends Component {
     static getInitialProps(ctx) {
@@ -10,9 +11,15 @@ export default class Create extends Component {
             isAuthenticated: isAuthenticated(ctx),
         };
     }
+
     state = {
         schemas: [],
+        name: '',
+        schemaId: null,
+        date: null,
+        error: '',
     };
+
     componentDidMount() {
         axios({
             method: 'get',
@@ -22,14 +29,58 @@ export default class Create extends Component {
         });
         //todo handle error
     }
+
+    handleChange = ({ currentTarget }) => {
+        const { name, value } = currentTarget;
+        this.setState({ [name]: value });
+    };
+
+    handleSubmit = async ev => {
+        const { name, date, schemaId } = this.state;
+
+        ev.preventDefault();
+
+        try {
+            if (!name.length || !date || !schemaId) {
+                throw new Error('Please fill out all of the fields');
+            }
+
+            const res = await axios({
+                method: 'post',
+                url: '/events/create',
+                data: {
+                    name,
+                    date,
+                    schemaId,
+                },
+            });
+
+            if (res) {
+                redirect('/manager/events');
+            }
+        } catch (e) {
+            this.setState({ error: e.message });
+        }
+    };
+
     render() {
         return (
             <ManagerContainer {...this.props}>
                 <h2>Create New Event</h2>
-                <form>
-                    <input type="text" placeholder="Event Name" />
-                    <input type="date" />
-                    <select>
+                {this.state.error}
+                <form onSubmit={this.handleSubmit}>
+                    <input
+                        name="name"
+                        type="text"
+                        placeholder="Event Name"
+                        onChange={this.handleChange}
+                    />
+                    <input
+                        name="date"
+                        type="date"
+                        onChange={this.handleChange}
+                    />
+                    <select name="schemaId" onChange={this.handleChange}>
                         <option>Select Schema</option>
                         {this.state.schemas.map(schema => {
                             return (
